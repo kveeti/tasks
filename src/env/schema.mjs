7112f4475db394abf68b0f131965f0b849994cd5
Veeti K <veeti@veetik.com>
2022-11-18 20:44:00 +0200

    more fix

--- src/env/schema.mjs
+++ src/env/schema.mjs
@@ -5,11 +5,11 @@ export const serverSchema = z.object({
 	DATABASE_URL: z.string(),
 	ENV: z.enum(["development", "preview", "production"]),
 	NEXTAUTH_SECRET:
-		process.env.NODE_ENV === "production" ? z.string().min(1) : z.string().min(1).optional(),
+		process.env.ENV === "production" ? z.string().min(1) : z.string().min(1).optional(),
 	NEXTAUTH_URL: z.preprocess(
 		(str) => process.env.VERCEL_URL ?? str,
 		process.env.VERCEL ? z.string() : z.string().url()
 	),
-	G_CLIENT_ID: process.env.NODE_ENV === "production" ? z.string() : z.string().optional(),
-	G_CLIENT_SECRET: process.env.NODE_ENV === "production" ? z.string() : z.string().optional(),
+	G_CLIENT_ID: process.env.ENV === "production" ? z.string() : z.string().optional(),
+	G_CLIENT_SECRET: process.env.ENV === "production" ? z.string() : z.string().optional(),
 });
