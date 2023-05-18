7feeeed4c3711ec7bf3f1ca360aaa2131f0b778e
Veeti K <veeti@veetik.com>
2023-05-18 21:47:12 +0300

    ?

--- apps/frontend/src/pages/AuthenticatedApp.tsx
+++ apps/frontend/src/pages/AuthenticatedApp.tsx
@@ -13,23 +13,16 @@ export function AuthenticatedApp() {
 
 	return (
 		<div className="fixed h-full w-full">
-			<AppRoutes />
+			<Routes>
+				<Route path="/app" element={<AppLayout />}>
+					<Route index element={<AppPage />} />
+					<Route path="stats" element={<StatsPage />} />
+					<Route path="tags" element={<TagsPage />} />
+					<Route path="settings" element={<AppSettingsPage />} />
+				</Route>
+				<Route path="*" element={<Navigate to="/app" />} />
+			</Routes>
 		</div>
-	);
-}
-
-function AppRoutes() {
-	return (
-		<Routes>
-			<Route path="/app" element={<AppLayout />}>
-				<Route index element={<AppPage />} />
-				<Route path="stats" element={<StatsPage />} />
-				<Route path="tags" element={<TagsPage />} />
-				<Route path="settings" element={<AppSettingsPage />} />
-			</Route>
-
-			<Route path="*" element={<Navigate to="/app" />} />
-		</Routes>
 	);
 }
 
--- apps/frontend/src/pages/UnauthenticatedApp.tsx
+++ apps/frontend/src/pages/UnauthenticatedApp.tsx
@@ -1,25 +1,20 @@
 import { Navigate, Route, Routes } from "react-router-dom";
+
 import { AuthLayout } from "./Auth/AuthLayout";
-import { LoginPage } from "./Auth/LoginPage";
 import { CallbackPage } from "./Auth/CallbackPage";
+import { LoginPage } from "./Auth/LoginPage";
 
 export function UnauthenticatedApp() {
 	return (
-		<div className="fixed w-full h-full">
-			<AppRoutes />
+		<div className="fixed h-full w-full">
+			<Routes>
+				<Route path="/auth" element={<AuthLayout />}>
+					<Route path="login" element={<LoginPage />} />
+					<Route path="callback" element={<CallbackPage />} />
+				</Route>
+
+				<Route path="*" element={<Navigate to="/auth/login" />} />
+			</Routes>
 		</div>
-	);
-}
-
-function AppRoutes() {
-	return (
-		<Routes>
-			<Route path="/auth" element={<AuthLayout />}>
-				<Route path="login" element={<LoginPage />} />
-				<Route path="callback" element={<CallbackPage />} />
-			</Route>
-
-			<Route path="*" element={<Navigate to="/auth/login" />} />
-		</Routes>
 	);
 }
