1c3aa1dee31d3e4e445417e127a606c0ffe319be
Veeti K <veeti@veetik.com>
2023-05-18 18:56:45 +0300

    1. stats page refactor

--- apps/frontend/src/pages/App/StatsPage/StatsPage.tsx
+++ apps/frontend/src/pages/App/StatsPage/StatsPage.tsx
@@ -1,142 +1,132 @@
 import { ResponsiveBar } from "@nivo/bar";
 import { ResponsivePie } from "@nivo/pie";
-import eachDayOfInterval from "date-fns/eachDayOfInterval";
-import endOfMonth from "date-fns/endOfMonth";
+import addMonths from "date-fns/addMonths";
 import format from "date-fns/format";
-import startOfMonth from "date-fns/startOfMonth";
-import type { ReactElement, ReactNode } from "react";
+import subMonths from "date-fns/subMonths";
+import { type ReactNode, useState } from "react";
 
 import { Button2 } from "@/Ui/Button";
 import { cn } from "@/utils/classNames";
 
-import { useMonthsTasks } from "./useMonthsTasks";
-
-function getBarData(thisMonthsDates: Date[]) {
-	return thisMonthsDates.map((date) => ({
-		id: format(date, "d.M"),
-		label: "coding",
-		// random between 1 and 24
-		value: Math.floor(Math.random() * 24) + 1,
-	}));
-}
+import { useStatsPageData } from "./UseStatsPageData";
 
 export function StatsPage() {
-	const data = useMonthsTasks({ date: new Date() });
-
-	const now = new Date();
-	const thisMonthsDates = eachDayOfInterval({
-		start: startOfMonth(now),
-		end: endOfMonth(now),
-	});
-
-	const barData = getBarData(thisMonthsDates);
-
-	const values = barData.map((d) => d.value);
-	const max = Math.max(...values);
-	const half = Number((max / 2).toFixed(0));
-	const yAxisValues = [0, half / 2, half, half + half / 2, max];
-
-	const xAxisValues = thisMonthsDates
-		.filter((date) => date.getDate() === 1 || date.getDate() % 6 === 0)
-		.map((d) => format(d, "d.M"));
+	const [date, setDate] = useState(new Date());
+	const { data, status } = useStatsPageData(date);
 
 	return (
-		<div className="flex flex-col pb-[10rem]">
-			{data && (
-				<>
-					<header className="sticky top-0 z-10 flex justify-between border-b-2 border-b-gray-800 bg-gray-900">
-						<h1 className="mb-4 pl-4 pt-4 text-2xl font-bold">Stats</h1>
+		<div className="flex flex-col">
+			<div className="sticky top-0 z-10 flex w-full gap-2 border-b-2 border-b-gray-800 bg-gray-900 p-2">
+				<Button2
+					className="w-max px-4 py-1"
+					onPress={() => setDate((prev) => subMonths(prev, 1))}
+				>
+					{"<"}
+				</Button2>
+				<Button2 className="w-full px-4 py-1">{format(date, "MMM YYY")}</Button2>
+				<Button2
+					className="w-max px-4 py-1"
+					onPress={() => setDate((prev) => addMonths(prev, 1))}
+				>
+					{">"}
+				</Button2>
+			</div>
 
-						<div className="flex gap-1 p-2">
-							<Button2 className="px-4 py-1">{"<"}</Button2>
-							<Button2 className="px-4 py-1">May</Button2>
-							<Button2 className="px-4 py-1">{">"}</Button2>
-						</div>
-					</header>
+			<div className="flex h-full flex-col gap-2 p-2">
+				<Card>
+					<Card.Header>Hours per day</Card.Header>
 
-					<div className="flex flex-col gap-2 p-2">
-						<Card>
-							<Card.Header>Hours per day</Card.Header>
-							<Card.Body className="h-[9rem]">
-								<ResponsiveBar
-									theme={{
-										grid: {
-											line: {
-												strokeWidth: 1,
-												stroke: "#636363",
-											},
+					<Card.Body className="h-[8rem]">
+						{status !== "data" ? (
+							<div className="flex h-full w-full items-center justify-center">
+								{status === "loading" ? "Loading..." : "No data"}
+							</div>
+						) : (
+							<ResponsiveBar
+								key={date.toString()}
+								theme={{
+									grid: {
+										line: {
+											strokeWidth: 1,
+											stroke: "#636363",
 										},
-										axis: {
-											ticks: {
-												text: {
-													fill: "#fff",
-												},
-												line: {
-													stroke: "#fff",
-												},
-											},
+									},
+									axis: {
+										ticks: {
+											text: { fill: "#fff" },
+											line: { stroke: "#fff" },
 										},
-									}}
-									colors={{ scheme: "nivo" }}
-									animate={false}
-									isInteractive={false}
-									enableLabel={false}
-									data={barData}
-									margin={{ top: 20, right: 0, bottom: 20, left: 30 }}
-									padding={0.2}
-									borderRadius={2}
-									indexBy="id"
-									axisTop={null}
-									axisRight={null}
-									axisBottom={{ tickValues: xAxisValues }}
-									axisLeft={{
-										tickValues: yAxisValues,
-										tickSize: 0,
-										format: (h) => `${h} h`,
-									}}
-									gridYValues={yAxisValues}
-								/>
-							</Card.Body>
-						</Card>
+									},
+								}}
+								colors={{ scheme: "nivo" }}
+								animate={false}
+								isInteractive={false}
+								enableLabel={false}
+								data={data.barData}
+								keys={[...data.monthsTags]}
+								margin={{ top: 20, right: 0, bottom: 20, left: 35 }}
+								padding={0.2}
+								borderRadius={2}
+								indexBy="id"
+								axisTop={null}
+								axisRight={null}
+								axisBottom={{
+									tickValues: data.xAxisValues,
+								}}
+								axisLeft={{
+									tickValues: data.yAxisValues,
+									tickSize: 0,
+									format: (h) => `${h} h`,
+								}}
+								gridYValues={data.yAxisValues}
+							/>
+						)}
+					</Card.Body>
+				</Card>
 
-						<Card>
-							<Card.Header>Tag distribution</Card.Header>
-							<Card.Body className="h-[14rem]">
-								<ResponsivePie
-									isInteractive={false}
-									animate={false}
-									data={data}
-									colors={{ scheme: "nivo" }}
-									margin={{ right: 100, left: 100, top: 20, bottom: 20 }}
-									innerRadius={0.55}
-									padAngle={1}
-									cornerRadius={3}
-									activeOuterRadiusOffset={8}
-									borderWidth={1}
-									borderColor={{
-										from: "color",
-										modifiers: [["darker", 0.2]],
-									}}
-									arcLabel={(e) => `${e.value} h`}
-									arcLinkLabel={({ label }) => {
-										const string = label.toString();
-										const isLonger = string.toString().length > 16;
-										const slicedLabel = isLonger
-											? string.toString().slice(0, 13).trim() + "..."
-											: string;
+				<Card>
+					<Card.Header>Tag distribution</Card.Header>
 
-										return slicedLabel;
-									}}
-									arcLinkLabelsColor={{ from: "color" }}
-									arcLinkLabelsDiagonalLength={8}
-									arcLinkLabelsStraightLength={4}
-									arcLinkLabelsTextColor="#fff"
-								/>
-							</Card.Body>
-						</Card>
-					</div>
-				</>
-			)}
+					<Card.Body className="h-[14rem] p-2">
+						{status !== "data" ? (
+							<div className="flex h-full w-full items-center justify-center">
+								{status === "loading" ? "Loading..." : "No data"}
+							</div>
+						) : (
+							<ResponsivePie
+								isInteractive={false}
+								animate={false}
+								data={data.pieData}
+								colors={{ scheme: "nivo" }}
+								margin={{ top: 25, bottom: 20, right: 90, left: 90 }}
+								innerRadius={0.5}
+								padAngle={1}
+								cornerRadius={3}
+								activeOuterRadiusOffset={8}
+								borderWidth={1}
+								borderColor={{
+									from: "color",
+									modifiers: [["darker", 0.2]],
+								}}
+								arcLabel={(e) => `${e.value} h`}
+								arcLinkLabel={({ label }) => {
+									const string = label.toString();
+									const isLonger = string.toString().length > 16;
+									const slicedLabel = isLonger
+										? string.toString().slice(0, 13).trim() + "..."
+										: string;
+
+									return slicedLabel;
+								}}
+								arcLinkLabelsColor={{ from: "color" }}
+								arcLinkLabelsDiagonalLength={8}
+								arcLinkLabelsStraightLength={4}
+								arcLinkLabelsTextColor="#fff"
+							/>
+						)}
+					</Card.Body>
+				</Card>
+			</div>
 		</div>
 	);
 }
@@ -161,7 +151,7 @@ function Body(props: { children: ReactNode; className?: string }) {
 	return (
 		<div
 			className={cn(
-				"h-full rounded-bl-xl rounded-br-xl border-b-2 border-l-2 border-r-2 border-gray-800 px-2 pb-2",
+				"rounded-bl-xl rounded-br-xl border-b-2 border-l-2 border-r-2 border-gray-800 p-2",
 				props.className
 			)}
 		>
---
+++ apps/frontend/src/pages/App/StatsPage/UseStatsPageData.ts
@@ -0,0 +1,137 @@
+import differenceInMinutes from "date-fns/differenceInMinutes";
+import eachDayOfInterval from "date-fns/eachDayOfInterval";
+import endOfMonth from "date-fns/endOfMonth";
+import format from "date-fns/format";
+import isSameDay from "date-fns/isSameDay";
+import isSameMonth from "date-fns/isSameMonth";
+import startOfMonth from "date-fns/startOfMonth";
+import { useEffect, useState } from "react";
+
+import { db } from "@/db/db";
+
+export function useStatsPageData(date: Date):
+	| {
+			status: "loading" | "no-data";
+			data: undefined;
+	  }
+	| {
+			status: "data";
+			data: NonNullable<Awaited<ReturnType<typeof getData>>>;
+	  } {
+	const [status, setStatus] = useState<"loading" | "no-data" | "data">("no-data");
+	const [stateData, setStateData] = useState<Awaited<ReturnType<typeof getData>>>(undefined);
+
+	useEffect(() => {
+		(async () => {
+			setStatus("loading");
+
+			const data = await getData(date);
+
+			if (!data) {
+				setStatus("no-data");
+			} else {
+				setStateData(data);
+				setStatus("data");
+			}
+		})();
+	}, [date]);
+
+	if (status === "data") {
+		return { status, data: stateData! };
+	} else {
+		return { status, data: undefined };
+	}
+}
+
+async function getData(date: Date) {
+	const monthsTasks = await db.tasks
+		.filter((task) => isSameMonth(date, task.createdAt))
+		.toArray();
+
+	if (!monthsTasks.length) {
+		return undefined;
+	}
+
+	const monthsTags = [...new Set(monthsTasks?.map((task) => task.tag.label) ?? [])];
+
+	const monthsDates = eachDayOfInterval({
+		start: startOfMonth(date),
+		end: endOfMonth(date),
+	});
+
+	const barData = monthsDates
+		.map((d) => ({
+			id: format(d, "d.M"),
+			date: d,
+			...monthsTags.reduce((acc, tag) => {
+				acc[tag] = 0;
+				return acc;
+			}, {} as Record<string, number>),
+		}))
+		.reduce((acc, curr) => {
+			const tasks = monthsTasks?.filter((task) => isSameDay(task.createdAt, curr.date));
+
+			if (!tasks) return acc;
+
+			tasks.forEach((task) => {
+				const tag = task.tag;
+
+				// @ts-expect-error hmm
+				curr[tag.label] += task.stoppedAt
+					? differenceInMinutes(task.stoppedAt, task.createdAt)
+					: differenceInMinutes(task.expiresAt, task.createdAt);
+			});
+
+			// @ts-expect-error hmm
+			acc.push(curr);
+
+			return acc;
+		}, [] as Record<string, number>[]);
+
+	const yMax = Math.max(
+		...barData.map((d) => {
+			const daysTotal = Object.values(d)
+				.map((v) => (typeof v === "number" ? v : 0))
+				.reduce((acc, curr) => curr + acc, 0);
+
+			return daysTotal;
+		})
+	);
+
+	const yHalf = Number((yMax / 2).toFixed(0));
+	const yAxisValues = [0, yHalf / 2, yHalf, yHalf + yHalf / 2, yMax];
+
+	const xHalf = monthsDates.length / 2;
+	const xFormatted = monthsDates.map((d) => format(d, "d.M"));
+	const xAxisValues = [
+		xFormatted.at(0),
+		xFormatted.at(xHalf / 2),
+		xFormatted.at(xHalf),
+		xFormatted.at(xHalf + xHalf / 2),
+		xFormatted.at(-1),
+	];
+
+	const pieData = monthsTasks?.reduce((acc, task) => {
+		const tag = task.tag;
+
+		const tagIndex = acc.findIndex((tag) => tag.id === task.tag.id);
+
+		const currentTaskMinutes = task.stoppedAt
+			? differenceInMinutes(task.stoppedAt, task.createdAt)
+			: differenceInMinutes(task.expiresAt, task.createdAt);
+
+		if (tagIndex === -1) {
+			acc.push({
+				id: tag.id,
+				label: tag.label,
+				value: currentTaskMinutes,
+			});
+		} else {
+			acc[tagIndex]!.value += currentTaskMinutes;
+		}
+
+		return acc;
+	}, [] as { id: string; label: string; value: number }[]);
+
+	return { barData, xAxisValues, yAxisValues, monthsTags, pieData };
+}
