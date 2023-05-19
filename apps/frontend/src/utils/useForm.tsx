354fa4cdb3d81c301e22157c74ae8c8fdad188cc
Veeti K <veeti@veetik.com>
2023-05-19 23:13:03 +0300

    handleSubmit to useForm

--- apps/frontend/src/utils/useForm.tsx
+++ apps/frontend/src/utils/useForm.tsx
@@ -1,5 +1,5 @@
 import { type ComponentProps } from "react";
-import { type UseFormProps, useForm as RHF_useForm, type FieldValues } from "react-hook-form";
+import { type FieldValues, useForm as RHF_useForm, type UseFormProps } from "react-hook-form";
 
 type Props<TFieldValues extends FieldValues = FieldValues> = {
 	onSubmit: (values: TFieldValues) => Promise<void> | void;
@@ -19,6 +19,7 @@ export function useForm<TFieldValues extends FieldValues = FieldValues>(
 
 	return {
 		...restOfForm,
+		handleSubmit: RHF_form_handleSubmit(inner_onSubmit),
 		Form: ({ children, ...rest }: Omit<ComponentProps<"form">, "onSubmit">) => (
 			<form onSubmit={RHF_form_handleSubmit(inner_onSubmit)} {...rest}>
 				{children}
