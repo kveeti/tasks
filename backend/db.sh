edfc89c8b67de5753ad3d19bb02ad4175c70a9e5
Veeti K <veeti@veetik.com>
2023-06-19 00:53:36 +0300

    Update db.sh

--- backend/db.sh
+++ backend/db.sh
@@ -1,6 +1,6 @@
 function check_envs() {
-    if [ -z "$DB_URL" ]; then
-        echo "DB_URL is not set"
+    if [ -z "$DATABASE_URL" ]; then
+        echo "DATABASE_URL is not set"
         exit 1
     fi
 }
@@ -8,8 +8,8 @@ function check_envs() {
 function initialize_db() {
     echo "Initializing database..."
 
-    psql $DB_URL -f ./sql/00-create-user-database-schema.sql
-    psql $DB_URL -f ./sql/01-create-tables.sql
+    # psql $DATABASE_URL -f ./sql/00-create-user-database-schema.sql #: railway does a db and a user for you
+    psql $DATABASE_URL -f ./sql/01-create-tables.sql
 
     echo "Database initialized!"
 }
@@ -17,18 +17,33 @@ function initialize_db() {
 function purge_db() {
     echo "Purging database..."
 
-    psql $DB_URL -c "DROP DATABASE IF EXISTS tasks_not_prod;"
+    psql $DATABASE_URL -c "DROP SCHEMA public CASCADE; CREATE SCHEMA public;"
 
     echo "Database purged!"
+}
+
+function generate_rust_code_with_sea_orm_cli() {
+    echo "Generating Rust db entities with sea-orm-cli..."
+
+    sea-orm-cli generate entity -o entity/src
+
+    echo "Rust db entities generated!"
 }
 
 function main() {
     check_envs
 
     if [ "$1" == "purge" ]; then
-        purge_db
-    else
+        purge_db 
+    else if [ "$1" == "init" ]; then
         initialize_db
+    else if [ "$1" == "gen" ]; then
+        generate_rust_code_with_sea_orm_cli
+    else
+        echo "Invalid argument"
+        exit 1
+    fi
+    fi
     fi
 }
 
