09edc3fec7ddf1d929088201c0cf2053e16a7799
Veeti K <veeti@veetik.com>
2023-06-21 00:33:44 +0300

    tasks page init

--- frontend/src/pages/App/AppTasksPage/AppTasksPage.tsx
+++ frontend/src/pages/App/AppTasksPage/AppTasksPage.tsx
@@ -1,10 +1,19 @@
 import { useButton } from "@react-aria/button";
 import { FocusRing } from "@react-aria/focus";
 import type { AriaButtonProps } from "@react-types/button";
+import addDays from "date-fns/addDays";
 import addHours from "date-fns/addHours";
+import format from "date-fns/format";
+import isSameDay from "date-fns/isSameDay";
+import isToday from "date-fns/isToday";
+import isTomorrow from "date-fns/isTomorrow";
+import isYesterday from "date-fns/isYesterday";
+import startOfDay from "date-fns/startOfDay";
+import subDays from "date-fns/subDays";
+import { useLiveQuery } from "dexie-react-hooks";
 import { motion, useAnimation } from "framer-motion";
-import { ChevronsUpDown, Plus } from "lucide-react";
-import { type ComponentProps, useRef, useState } from "react";
+import { ChevronLeft, ChevronRight, ChevronsUpDown, Plus } from "lucide-react";
+import { type ComponentProps, useEffect, useRef, useState } from "react";
 import colors from "tailwindcss/colors";
 import { z } from "zod";
 
@@ -12,7 +21,7 @@ import { Input } from "@/Ui/Input";
 import { Label } from "@/Ui/Label";
 import { Modal } from "@/Ui/Modal";
 import { Button, SelectButton } from "@/Ui/NewButton";
-import { db } from "@/db/db";
+import { type DbTask, addNotSynced, db } from "@/db/db";
 import { cn } from "@/utils/classNames";
 import { createId } from "@/utils/createId";
 import { sleep } from "@/utils/sleep";
@@ -22,20 +31,138 @@ import { useTimerContext } from "../TimerContext";
 import { WithAnimation } from "../WithAnimation";
 
 export function AppTasksPage() {
+	const [selectedDay, setSelectedDay] = useState(startOfDay(new Date()));
+	const formattedSelectedDay = isToday(selectedDay)
+		? "today"
+		: isYesterday(selectedDay)
+		? "yesterday"
+		: isTomorrow(selectedDay)
+		? "tomorrow"
+		: format(selectedDay, "EEE, MMMM do");
+
+	function scrollTimeFrame(direction: "left" | "right") {
+		setSelectedDay((day) => {
+			if (direction === "right") {
+				return addDays(day, 1);
+			} else {
+				return subDays(day, 1);
+			}
+		});
+	}
+
+	const { dbTags } = useTimerContext();
+	const dbTasks = useLiveQuery(
+		() =>
+			db.tasks.filter((t) => !t.deleted_at && isSameDay(t.started_at, selectedDay)).toArray(),
+		[selectedDay]
+	);
+
+	const dbTasksWithTags = dbTasks?.map((task) => ({
+		...task,
+		tag: dbTags?.find((tag) => tag.id === task.tag_id),
+	}));
+
 	return (
 		<WithAnimation>
-			<div className=" flex h-full w-full flex-col gap-2">
-				<div className="flex items-center justify-between gap-4">
-					<div>
-						<h1 className="px-4 pt-4 text-2xl font-bold">tasks</h1>
-					</div>
+			<div className="flex h-full w-full flex-col">
+				<div className="flex items-center justify-between gap-4 px-4 pt-4">
+					<h1 className="text-2xl font-bold">tasks</h1>
 
-					<div className="px-4 pt-4">
-						<NewTask />
+					<NewTask />
+				</div>
+
+				<div className="flex h-full flex-col gap-2 overflow-auto p-4">
+					{dbTasksWithTags?.map((task) => (
+						<Task>
+							<div className="flex w-full items-center justify-between gap-3">
+								<div className="flex items-center gap-3">
+									<div
+										className="h-3 w-3 rounded-full"
+										style={{ backgroundColor: task.tag.color }}
+									/>
+
+									<span>{task.tag.label}</span>
+								</div>
+
+								<span className="text-gray-400">
+									{format(task.started_at, "HH:mm")} -{" "}
+									{format(task.stopped_at ?? task.expires_at, "HH:mm")}
+								</span>
+							</div>
+						</Task>
+					))}
+				</div>
+
+				<div className="border-t border-t-gray-800 bg-gray-900 p-4">
+					<div className="flex w-full justify-between gap-4">
+						<Button className="p-2" onPress={() => scrollTimeFrame("left")}>
+							<ChevronLeft />
+						</Button>
+
+						<span className="flex w-full items-center justify-center rounded-xl bg-gray-600 px-4 py-2">
+							{formattedSelectedDay}
+						</span>
+
+						<Button className="p-2" onPress={() => scrollTimeFrame("right")}>
+							<ChevronRight />
+						</Button>
 					</div>
 				</div>
 			</div>
 		</WithAnimation>
+	);
+}
+
+function Task(props: ComponentProps<"button"> & AriaButtonProps & { isCreatedTag?: boolean }) {
+	const ref = useRef<HTMLButtonElement | null>(null);
+	const controls = useAnimation();
+
+	const aria = useButton(
+		{
+			...props,
+			onPress: async (e) => {
+				controls.set({ backgroundColor: colors.neutral[800] });
+
+				controls.start({
+					backgroundColor: "rgb(10 10 10 / 0.5)",
+					transition: { duration: 0.4 },
+				});
+
+				props.onPress?.(e);
+			},
+			// @ts-expect-error undocumented prop
+			preventFocusOnPress: true,
+		},
+		ref
+	);
+
+	useEffect(() => {
+		if (!props.isCreatedTag) return;
+
+		controls.set({ backgroundColor: colors.neutral[700] });
+
+		controls.start({
+			backgroundColor: "rgb(10 10 10 / 0.5)",
+			transition: { duration: 0.5 },
+		});
+		// eslint-disable-next-line react-hooks/exhaustive-deps
+	}, []);
+
+	return (
+		<FocusRing focusRingClass="outline-gray-300">
+			{/* @ts-expect-error dont know how to fix this */}
+			<motion.button
+				{...aria.buttonProps}
+				ref={ref}
+				animate={controls}
+				className={cn(
+					"flex w-full cursor-default items-center gap-4 rounded-xl bg-gray-950/50 p-4 outline-none outline-2 outline-offset-2",
+					props.className
+				)}
+			>
+				{props.children}
+			</motion.button>
+		</FocusRing>
 	);
 }
 
@@ -58,7 +185,7 @@ function NewTask() {
 			const startDate = new Date(values.start);
 			const endDate = addHours(startDate, values.duration);
 
-			await db.tasks.add({
+			const newTask: DbTask = {
 				id: createId(),
 				tag_id: values.tagId,
 				started_at: startDate,
@@ -67,7 +194,9 @@ function NewTask() {
 				deleted_at: null,
 				updated_at: new Date(),
 				created_at: new Date(),
-			});
+			};
+
+			await Promise.all([db.tasks.add(newTask), addNotSynced(newTask.id, "task")]);
 
 			addTasksForm.reset();
 			setIsOpen(false);
