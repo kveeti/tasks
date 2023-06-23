5cdc34029150a6f746ed3b906fe653b4a85770fd
Veeti K <veeti@veetik.com>
2023-06-23 05:31:36 +0300

    Update useNotifications.tsx

--- frontend/src/utils/useNotifications.tsx
+++ frontend/src/utils/useNotifications.tsx
@@ -1,12 +1,9 @@
-import { useMutation } from "@tanstack/react-query";
 import { useEffect } from "react";
 
 import { apiRequest } from "./api/apiRequest";
 import { urlBase64ToUint8Array } from "./urlBase64ToUint8Array";
 
 export function useNotifications() {
-	const addNotifSubMutation = useAddNotifSubMutation();
-
 	useEffect(() => {
 		(async () => {
 			if (Notification.permission !== "granted") {
@@ -19,8 +16,8 @@ export function useNotifications() {
 			await navigator.serviceWorker.ready;
 
 			const sub = await reg.pushManager.getSubscription();
-			if (sub) {
-				localStorage.setItem("notifs-enabled", "1");
+			if (!sub) {
+				console.debug("useNotifications no subscription");
 				return;
 			}
 
@@ -35,23 +32,15 @@ export function useNotifications() {
 				return;
 			}
 
-			await addNotifSubMutation
-				.mutateAsync({
+			apiRequest<void>({
+				method: "POST",
+				path: "/notif-subs",
+				body: {
 					endpoint: subJson.endpoint,
 					auth: subJson.keys.auth,
 					p256dh: subJson.keys.p256dh,
-				})
-				.catch(() => null);
+				},
+			}).catch(() => null);
 		})();
 	}, []);
-}
-
-function useAddNotifSubMutation() {
-	return useMutation((body: { endpoint: string; auth: string; p256dh: string }) =>
-		apiRequest<void>({
-			method: "POST",
-			path: "/notif-subs",
-			body,
-		})
-	);
 }
