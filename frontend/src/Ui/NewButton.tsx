9536c74ce3d751d51341bf11bbf7d0ca98130556
Veeti K <veeti@veetik.com>
2023-06-25 03:16:39 +0300

    Update NewButton.tsx

--- frontend/src/Ui/NewButton.tsx
+++ frontend/src/Ui/NewButton.tsx
@@ -49,7 +49,7 @@ export function Button(props: { className?: string; isSecondary?: boolean } & Ar
 				ref={ref}
 				animate={controls}
 				className={twMerge(
-					"flex cursor-default select-none items-center justify-center gap-2 rounded-xl p-3 outline-none outline-2 outline-offset-2 transition-[outline,opacity] duration-200 disabled:opacity-40",
+					"flex cursor-default select-none touch-none items-center justify-center gap-2 rounded-xl p-3 outline-none outline-2 outline-offset-2 transition-[outline,opacity] duration-200 disabled:opacity-40",
 					props.isSecondary ? "bg-gray-700" : "bg-gray-600",
 					props.className
 				)}
@@ -104,7 +104,7 @@ export function SelectButton(props: { className?: string } & AriaButtonProps) {
 				ref={ref}
 				animate={controls}
 				className={twMerge(
-					"flex cursor-default select-none items-center justify-between gap-2 rounded-xl border border-gray-600 bg-gray-800 p-3 outline-none outline-2 outline-offset-2 transition-[outline,opacity] duration-200 disabled:opacity-40",
+					"flex cursor-default select-none touch-none items-center justify-between gap-2 rounded-xl border border-gray-600 bg-gray-800 p-3 outline-none outline-2 outline-offset-2 transition-[outline,opacity] duration-200 disabled:opacity-40",
 					props.className
 				)}
 			>
