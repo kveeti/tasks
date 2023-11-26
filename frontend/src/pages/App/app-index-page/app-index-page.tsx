1cb7a380f706800eb7f203de34e3856b5d0b1a8e
Veeti K <veeti@veetik.com>
2023-11-26 22:58:19 +0200

    use pagelayout on index

--- frontend/src/pages/App/app-index-page/app-index-page.tsx
+++ frontend/src/pages/App/app-index-page/app-index-page.tsx
@@ -1,6 +1,6 @@
 import { Counter } from "@/Ui/Counter";
+import { PageLayout } from "@/components/page-layout";
 
-import { WithAnimation } from "../../../components/with-animation";
 import { useTimerContext } from "../timer-context";
 import { SelectTag } from "./select-tag";
 import { TaskControls } from "./task-controls";
@@ -10,7 +10,7 @@ export function AppIndexPage() {
 	const { form, onGoingSeconds } = useTimerContext();
 
 	return (
-		<WithAnimation>
+		<PageLayout>
 			<div className="flex h-full w-full flex-col items-center justify-center p-8">
 				<span className="rounded-3xl border border-gray-800 bg-gray-950/50 p-4 font-bold text-gray-50">
 					<Counter seconds={onGoingSeconds ? onGoingSeconds : form.watch("seconds")} />
@@ -22,6 +22,6 @@ export function AppIndexPage() {
 
 				<TaskControls />
 			</div>
-		</WithAnimation>
+		</PageLayout>
 	);
 }
