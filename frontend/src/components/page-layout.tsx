86ec730a4b6f3ee607dfa9ad57784cfa5edf1736
Veeti K <veeti@veetik.com>
2023-11-26 22:58:44 +0200

    remove animation on page layout

--- frontend/src/components/page-layout.tsx
+++ frontend/src/components/page-layout.tsx
@@ -1,30 +1,15 @@
-import { motion } from "framer-motion";
 import { type ComponentPropsWithoutRef, type ReactNode, forwardRef } from "react";
 
 import { cn } from "@/lib/utils";
 
 export function PageLayout({ children }: { children: ReactNode }) {
-	return (
-		<motion.div
-			key={location.pathname}
-			className="h-full w-full flex flex-col"
-			initial={{ opacity: 0 }}
-			animate={{ opacity: 1 }}
-			exit={{ opacity: 0 }}
-		>
-			{children}
-		</motion.div>
-	);
+	return <div className="h-full w-full flex flex-col">{children}</div>;
 }
 
 const Title = forwardRef<HTMLHeadingElement, ComponentPropsWithoutRef<"h1">>(
 	({ className, ...props }, ref) => {
 		return (
-			<h1
-				ref={ref}
-				{...props}
-				className={cn("text-xl font-bold border-b p-4 shadow-lg z-10", className)}
-			/>
+			<h1 ref={ref} {...props} className={cn("text-xl font-bold border-b p-4", className)} />
 		);
 	}
 );
