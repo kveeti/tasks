19b7358e6a46825652401c7a0a622b0bc1482a63
Veeti K <veeti@veetik.com>
2023-12-01 06:47:43 +0200

    sql

--- backend/db/migrations/20231122174909_init.up.sql
+++ backend/db/migrations/20231122174909_init.up.sql
@@ -1,15 +1,17 @@
 CREATE TABLE "users" (
     "id" VARCHAR(26) PRIMARY KEY,
     "email" VARCHAR(255) UNIQUE NOT NULL,
-    "preferences" BIGINT NOT NULL,
+    "preferences" BIGINT NOT NULL DEFAULT 0,
     "joined_at" TIMESTAMPTZ NOT NULL
 );
+CREATE INDEX idx_users_email ON "users"("email")
 
 CREATE TABLE "sessions" (
     "id" VARCHAR(26) PRIMARY KEY,
     "user_id" VARCHAR(26) NOT NULL REFERENCES "users"("id") ON DELETE CASCADE,
     "expires_at" TIMESTAMPTZ NOT NULL
 );
+CREATE INDEX idx_sessions_user_id ON "sessions"("user_id");
  
 CREATE TABLE "tags" (
     "id" VARCHAR(26) PRIMARY KEY,
@@ -17,6 +19,8 @@ CREATE TABLE "tags" (
     "label" VARCHAR(255) NOT NULL,
     "color" VARCHAR(7) NOT NULL
 );
+CREATE INDEX idx_tags_user_id ON "tags"("user_id");
+CREATE INDEX idx_tags_label ON "tags"("label");
 
 CREATE TABLE "tasks" (
     "id" VARCHAR(26) PRIMARY KEY,
@@ -27,6 +31,9 @@ CREATE TABLE "tasks" (
     "start_at" TIMESTAMPTZ NOT NULL,
     "end_at" TIMESTAMPTZ NOT NULL
 );
+CREATE INDEX idx_tasks_user_id ON "tasks"("user_id");
+CREATE INDEX idx_tasks_start_at ON "tasks"("start_at");
+CREATE INDEX idx_tasks_end_at ON "tasks"("end_at");
 
 CREATE TABLE "notification_subs" (
     "id" VARCHAR(26) PRIMARY KEY,
@@ -35,6 +42,7 @@ CREATE TABLE "notification_subs" (
     "p256dh" VARCHAR(255) NOT NULL,
     "auth" VARCHAR(255) NOT NULL
 );
+CREATE INDEX idx_notification_subs_user_id ON "notification_subs"("user_id");
 
 CREATE TABLE "notifications" (
     "id" VARCHAR(26) PRIMARY KEY,
