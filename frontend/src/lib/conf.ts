0f98683b5ab533c6a778bb12415be8157a13e161
Veeti K <veeti@veetik.com>
2024-07-05 02:44:50 +0300

    frontend: switch to rsbuild (and pnpm) (#94)

--- frontend/src/lib/conf.ts
+++ frontend/src/lib/conf.ts
@@ -1,5 +1,5 @@
 export const conf = {
-	API_URL: import.meta.env.VITE_APP_API_URL + "/api/v1",
-	VAPID_PUBLIC_KEY: import.meta.env.VAPID_PUBLIC_KEY,
-	IS_PROD: import.meta.env.PROD,
+	API_URL: process.env.PUBLIC_API_URL + "/api/v1",
+	VAPID_PUBLIC_KEY: process.env.PUBLIC_VAPID_PUBLIC_KEY!,
+	IS_PROD: process.env.NODE_ENV === "production",
 };
